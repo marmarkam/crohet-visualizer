3c9b91dbac95fff801334302309a9e113cbcd47c
Maryam <marykhan99@gmail.com>
2026-06-01 19:26:53 -0500

    started parser, got down constants

---
+++ venv/core/parser.py
@@ -0,0 +1,37 @@
+from core.models import Pattern, Round, Segment, Stitch
+
+'''
+Parser will break down input to output segment objects
+'''
+
+#constants
+SUPPORTED_STITCHES = {"sc", "inc", "dec"}
+
+UNSUPPORTED_STITCHES = {
+    "ch": "chaining is not yet supported",
+    "sl": "slip stitches are not yet supported",
+    "dc": "double crochet is not yet supported",
+    "hdc": "half double crochet is not yet supported",
+    "sk": "skipping stitches is not yet supported",
+}
+
+#magic ring assumption
+MAGIC_RING_COUNT = 6 #default starting stitches
+
+def create_magic_ring() -> Round:
+    '''
+    creates default starting round of 6 sc mr
+    all amigurumi parts are assumed to begin this mr
+    can be changed by user
+    '''
+    stitches = [Stitch("sc") for _ in range(MAGIC_RING_COUNT)]
+    stitches[0].is_round_start = True
+    segment = Segment(stitches, repeats=1)
+    return Round([segment], round_number = 1)
+
+#exceptions
+class UnsupportedStitchError(Exception):
+    pass
+
+#pure string -> stitch objects
+# def parse_stitch(token: str) -> Stitch:
